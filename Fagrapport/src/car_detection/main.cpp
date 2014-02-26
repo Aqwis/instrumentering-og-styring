6c02418bebb34a90e66e25709ee98c3cc2c9544f
Trygve Bertelsen Wiig <trygvebw@gmail.com>
2014-02-26 14:33:10 +0100

    Ting

--- Fagrapport/src/car_detection/main.cpp
+++ Fagrapport/src/car_detection/main.cpp
@@ -1,3 +1,19 @@
 #include <iostream>
-#include <math>
-#include <
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    Mat image;
+    image = imread("cars1.jpg", CV_LOAD_IMAGE_COLOR);
+
+    if (!image.data) {
+        cout << "Could not open image." << endl;
+    }
+
+    return 0;
+}
